eb315b7bdab2f57dbc1f4343333ea6766da66589
Karthik P N <karthikpnaragunda77@gmail.com>
2026-08-18 17:31:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,16 @@ class Config:
     # =========================
     # MongoDB
     # =========================
+
     MONGO_URI = os.getenv("MONGO_URI")
+
+    # Database name
+    DB_NAME = os.getenv("DB_NAME", "sagar_hotel")
 
     # =========================
     # Flask
     # =========================
+
     SECRET_KEY = os.getenv(
         "SECRET_KEY",
         "sagar-hotel-secret-key"
@@ -19,6 +24,7 @@ class Config:
     # =========================
     # Upload Folder
     # =========================
+
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     UPLOAD_FOLDER = os.path.join(
@@ -30,6 +36,7 @@ class Config:
     # =========================
     # QR Code Folder
     # =========================
+
     QRCODE_FOLDER = os.path.join(
         BASE_DIR,
         "static",
@@ -39,10 +46,13 @@ class Config:
     # =========================
     # MongoDB Options
     # =========================
+
     MONGO_OPTIONS = {
         "tls": True,
         "tlsCAFile": certifi.where(),
         "serverSelectionTimeoutMS": 10000,
         "connectTimeoutMS": 20000,
-        "socketTimeoutMS": 20000
+        "socketTimeoutMS": 20000,
+        "retryWrites": True,
+        "retryReads": True
     }
