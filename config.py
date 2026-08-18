59c45ecde2f0eb3f01b7ad1659cddb46b967f572
Karthik P N <karthikpnaragunda77@gmail.com>
2026-08-18 17:27:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,42 @@ import certifi
 
 
 class Config:
+    # =========================
+    # MongoDB
+    # =========================
     MONGO_URI = os.getenv("MONGO_URI")
 
+    # =========================
+    # Flask
+    # =========================
     SECRET_KEY = os.getenv(
         "SECRET_KEY",
         "sagar-hotel-secret-key"
     )
 
+    # =========================
+    # Upload Folder
+    # =========================
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
     UPLOAD_FOLDER = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
+        BASE_DIR,
         "static",
         "uploads"
     )
 
+    # =========================
+    # QR Code Folder
+    # =========================
+    QRCODE_FOLDER = os.path.join(
+        BASE_DIR,
+        "static",
+        "qrcodes"
+    )
+
+    # =========================
+    # MongoDB Options
+    # =========================
     MONGO_OPTIONS = {
         "tls": True,
         "tlsCAFile": certifi.where(),
